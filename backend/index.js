a657586efd93c06227d68af3395a32dd4c8766de
numantariq435 <numantariq435@gmail.com>
2021-05-04 22:14:29 +0500

    Authentication and Authorization

--- backend/index.js
+++ backend/index.js
@@ -21,18 +21,9 @@ app.use('', (req, res, next) => {
   return res.status(200).send('<h1>Express server is running here.</h1>');
 });
 
-
 // Express app listener
 const server = http.createServer(app);
 const port = process.env.port || 4001;
 server.listen(port, () => {
   console.log(`Express server is running on port: ${port}`);
 })
-
-function apiCall(resolve, reject) {
-  if(Math.floor(Math.random()*1)) {
-    resolve({ message: 'OK'});
-  } else {
-    reject({ message: 'Rejected'});
-  }
-}
